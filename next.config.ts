30ed66535dbdda54aa65186055f99f2d5633de58
Varinder Singh <varindersingh14.vs@gmail.com>
2025-03-20 10:34:00 +0530

    testing

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const isDev = process.env.NODE_ENV !== "production";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? "/portfolio/" : "",
-  basePath: isProd ? "/portfolio" : "",
+  assetPrefix: !isDev ? "/portfolio/" : "",
+  basePath: !isDev ? "/portfolio" : "",
   output: "export",
 };
 
